9a45a0f2df77d492922835bd3867afcbda92ce98
David Blader <dblader79@gmail.com>
2024-04-10 13:39:04 -0400

    cleanup, add abort if sort file creation fails

--- highest_score.js
+++ highest_score.js
@@ -1,42 +1,43 @@
 const fs = require('fs');
+const readline = require('readline');
 const { execSync } = require('child_process');
 
 // Get arguments from CLI
 const { recordsFilePath, totalScores } = getCLIArgs();
 
-// Surprise! The DevOps guy likes using shell commands :-)
-// sort -rn sorts in reverse numerical order, head -n limits us to just the top results we're interested in
-// We pass both of these to be subshell executed by echo -n to remove annoying newline character appended by head
-let rawOutput = execSync(`echo -n "$(sort -rn ${recordsFilePath} | head -n ${totalScores})"`, (err) => {
+const sortedFile = "./sorted_score_recs.data"
+// sort -rn sorts in reverse numerical order
+execSync(`sort -rn ${recordsFilePath} -o ${sortedFile}`, (err) => {
     if (err) {
-        console.error(`Error creating sort file: ${err}`)
-    }
-}).toString();
-
-// Parse output
-let output = [];
-rawOutput.split("\n").forEach((row) => {
-    // Splits line by first :, leaving JSON string intact
-    const splitRow = row.split(new RegExp('\:(.*)'), 2);
-    const score = splitRow[0];
-    const json  = splitRow[1];
-    // Validate JSON, throw error if invalid
-    try {
-        const id = JSON.parse(json).id;
-        if (id === undefined) {
-            console.error(`Invalid JSON format. JSON object is missing id attribute.\n${json}`);
-            process.exit(1);
-        }
-    } catch (error) {
-        console.error(`Invalid JSON format. No JSON object could be decoded.\n${json}\n${error}`);
+        console.error(`Error creating sort file: ${err}`);
         process.exit(1);
     }
-    output.push({
-        score,
-        id: JSON.parse(json).id
-    })
 });
-console.log(output);
+
+const sortedFileStream = fs.createReadStream(sortedFile);
+const sortedFileLines  =  readline.createInterface({
+    input: sortedFileStream,
+    crlfDelay: Infinity
+});
+const scoreToId = {};
+
+sortedFileLines.on('line', (row) => {
+    const data = processRowString(row);
+     // Note re: requirement:
+     // "Scores can repeat, but you should only count the `id` of the _last_ line processed as the "winning" `id`."
+     // Here I am simply choosing to overwrite at the `score` index, mapping to the object id
+     // Because we used coreutils sort command, we cannot guarantee that the "correct" order of the identical scores was preserved in the output file
+     // In a real application, I would seek clarification on this requirement and perhaps use the `date` field in the object to determine which gets selected as the winner
+     scoreToId[data.score] = data.id;
+
+     // Once our map reaches the desired number of scores, output as an array, cleanup, and exit
+     if (Object.keys(scoreToId).length == totalScores) {
+        output(scoreToId);
+        cleanupAndExit();
+     };
+});
+
+// Helper functions
 
 // Returns and validates CLI arguments
 function getCLIArgs() {
@@ -65,4 +66,40 @@ function getCLIArgs() {
         recordsFilePath,
         totalScores
     };
+}
+
+// Returns object with score and object payload from raw string input 
+function processRowString(row) {
+    // Splits line by first :, leaving JSON string intact
+    const splitRow = row.split(new RegExp('\:(.*)'), 2);
+    const score = splitRow[0];
+    const json  = splitRow[1];
+    // Validate JSON, throw error if invalid
+    try {
+        const id = JSON.parse(json).id;
+        if (id === undefined) {
+            console.error(`Invalid JSON format. JSON object is missing id attribute.\n${json}`);
+            process.exit(1);
+        }
+    } catch (error) {
+        console.error(`Invalid JSON format. No JSON object could be decoded.\n${json}\n${error}`);
+        process.exit(1);
+    }
+
+    return { score, id: JSON.parse(json).id };
+}
+
+// Outputs scores and IDs and exits on successful execution
+function output(scoreToId) {
+    const result = [];
+    Object.keys(scoreToId).forEach((score) => result.push({score, id: scoreToId[score]}));
+    // Sort array so higher scores are indexed first
+    result.sort((score1, score2) => score2.score - score1.score);
+    console.log(result);
+}
+
+// Deletes sorted data file
+function cleanupAndExit() {
+    execSync(`rm ${sortedFile}`);
+    process.exit(0);
 }
